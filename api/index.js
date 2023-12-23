464cae60d02c0ef4abd0f5b496a10c969599ce45
Ganesh <ghattiganesh8@gmail.com>
2023-12-23 13:53:44 +0530

    main

--- api/index.js
+++ api/index.js
@@ -1,18 +1,3 @@
-const express = require("express");
-const connectdatabase = require("../config/database");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const userroutes = require("../routes/User");
-require("dotenv").config();
+import app from "../app"
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(userroutes);
-
-connectdatabase();
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+export default app;
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectdatabase = require("../config/database");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userroutes = require("../routes/User");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(userroutes);
+
+connectdatabase();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

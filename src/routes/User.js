fc72ea61c02f482f20b337187783be32a5b9cf2f
Ganesh <ghattiganesh8@gmail.com>
2024-01-13 23:37:50 +0530

    updated now

--- src/routes/User.js
+++ src/routes/User.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {register, auth, login, form}=require("../Controller/User")
 const requireAuth=require('../middleware/User')
 
-router.route("/register").post(register);
-router.route("/auth").post(auth);
-router.route("/login").post(login);
+// router.route("/register").post(register);
+// router.route("/auth").post(auth);
+// router.route("/login").post(login);
 router.route("/form").post(form);
 
 module.exports = router;
